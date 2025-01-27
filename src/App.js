550ff44b6aa4cc9db9fbd2b7039e9d106f35d7e2
alswn-03 <soojooyong@gmail.com>
2025-01-27 23:15:04 +0900

    model 2

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import "./Main.css";
 
 function App() {
   const [prompt, setPrompt] = useState("");
-
   const [genderPrompt, setGenderPrompt] = useState(""); 
   const [facePrompt, setFacePrompt] = useState(""); 
 
   const [image, setImage] = useState(null);
   const [preview, setPreview] = useState(null);
+  const [generatedImage1, setGeneratedImage1] = useState(null);
   const [generatedImage, setGeneratedImage] = useState(null);
   const [loading, setLoading] = useState(false);
 
@@ -76,12 +76,13 @@ function App() {
       reader.onload = async () => {
         const base64Image1 = reader.result.split(",")[1]; // base64Image
 
-        // 2. lora, controlnet 추가
+  
+      // 1. Prepare payload1 : lora, controlnet able
+
         const prompt1 = `${prompt}`; //`emoji, ${prompt}, <lora:memoji:1>`
         const scale1 = 1;
         const controlNet1 = "True";
-  
-        // 3. Prepare payload1
+
         const payload1 = {
           seed: -1,
           init_images: [base64Image1],
@@ -113,38 +114,86 @@ function App() {
              }
           }
         };
+        console.log("payload1")
         console.log(payload1);
 
-        try {
+        try { // try 1
           const response = await axios.post("/sdapi/v1/img2img", payload1, {
             headers: {
               "Content-Type": "application/json",
             },
           });
-  
-          setGeneratedImage(response.data.images[0]);
+          setGeneratedImage1(response.data.images[0]);
+
+          console.log("first generated image");
           console.log(response.data);
+  
+          // 2. Prepare payload2 : lora, controlnet disable, upscaling
+          const base64Image2 = response.data.images[0]; 
+          const prompt2 = `${prompt}`; //`emoji, ${prompt}`
+          const scale2 = 1.5;
+          const controlNet2 = "False";
+
+          const payload2 = {
+            seed: -1,
+            init_images: [base64Image2],
+            prompt: prompt2,
+            negative_prompt: "Pixelated design, distorted facial features, blurry rendering, overly detailed skin texture, animal-like characteristics, sharp or harsh expressions, low-quality resolution, unrecognizable face, unnatural colors, 3D effects",
+            strength: 0.7,
+            steps: 50,
+            cfg_scale: 7.0,
+
+            // upscaling
+            //width: width * scale2, 
+            //height: height * scale2,
+            //resize_mode: 0,
+
+            // controlnet
+            alwayson_scri: { "ControlNet": { "args": [
+                      {
+                          "enabled": controlNet2,
+                          "guidance_end": 1,
+                          "guidance_start": 0,
+                          "model": "diffusers_xl_canny_full [2b69fca4]",
+                          "module": "canny",
+                          "pixel_perfect": "True",
+                          //"processor_res": 512,
+                          //"resize_mode": "Crop and Resize",
+                          "weight": 1
+                      }
+                  ]
+              }
+            }
+          };
+          console.log("payload2")
+          console.log(payload2);
+
+          //try 2
+          const response2 = await axios.post("/sdapi/v1/img2img", payload2, {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+  
+          setGeneratedImage(response2.data.images[0]);
+          console.log("second generated image");
+          console.log(response2.data);
         } catch (error) {
-          console.error(error);
+          console.error("Error during image generation:", error);
           alert("Failed to generate image. Check console for details.");
         }
       };
       reader.readAsDataURL(image);
     } catch (error) {
-        console.error(error);
-        alert("Failed to convert image to Base64. Check console for details.");
+    console.error(error);
+    alert("Failed to convert image to Base64. Check console for details.");
     } finally {
-      setLoading(false);
+    setLoading(false);
     }
-
-    // MODEL 2차
-    
-
-
-
-
-    // moel done
   };
+
+
+
  
   return (<div style={{ textAlign: "center", padding: "20px" }}>
     <h1>Stable Diffusion img2img</h1>
